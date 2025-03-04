1636548b92bf9f7b1d013a9be0ff35368d3d8689
Josef Hak <jhak@mirantis.com>
2025-03-04 16:03:37 +0100

    Add page metadata validation

--- mkdocs/gen_app_pages.py
+++ mkdocs/gen_app_pages.py
@@ -2,12 +2,21 @@ import os
 import yaml
 from jinja2 import Template
 
+required_fields = ['title', 'tags', 'summary', 'logo', 'description', 'install_code', 'verify_code', 'deploy_code',
+                   'support_link', 'doc_link']
+
 def changed(file, content):
     if os.path.exists(file):
         with open(file, 'r', encoding='utf-8') as f:
             if f.read() == content:
                 return False
     return True
+
+
+def validate_metadata(file: str, data: dict):
+    for required_field in required_fields:
+        if required_field not in data:
+            raise Exception(f"Required field '{required_field}' not found in {file}")
 
 def generate_apps():
     apps_dir = 'mkdocs/apps'
@@ -27,6 +36,7 @@ def generate_apps():
         if os.path.isdir(app_path) and os.path.exists(data_file):
             with open(data_file, 'r', encoding='utf-8') as f:
                 metadata = yaml.safe_load(f)
+                validate_metadata(data_file, metadata)
 
             # Render the template with metadata
             rendered_md = template.render(**metadata)
@@ -34,7 +44,6 @@ def generate_apps():
             if changed(md_file, rendered_md):
                 # Write the generated markdown
                 with open(md_file, 'w', encoding='utf-8') as f:
-                    print("render file")
                     f.write(rendered_md)
 
 generate_apps()
