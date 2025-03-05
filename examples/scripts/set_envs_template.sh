fbe24a7fb88e4cbe5d98aee00839778929bbb2ce
Josef Hak <jhak@mirantis.com>
2025-03-05 14:56:41 +0100

    Add Azure envs to set_envs_template.sh

--- examples/scripts/set_envs_template.sh
+++ examples/scripts/set_envs_template.sh
@@ -2,6 +2,11 @@
 export AWS_ACCESS_KEY_ID="FILL"
 export AWS_SECRET_ACCESS_KEY="FILL"
 
+# Azure k0rdent provider
+export AZURE_SP_PASSWORD="FILL"
+export AZURE_SP_APP_ID="FILL"
+export AZURE_SP_TENANT_ID="FILL"
+
 # Try run in terminal $CHROME_CMD, fix if it doesn't work
 export CHROME_CMD="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 export USE_CHROME=yes # set "no" to disable
